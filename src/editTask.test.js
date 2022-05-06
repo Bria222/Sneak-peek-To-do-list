ed92b6b0943d256910ee7457cec524c0ae84a6ec
MR BREE <briantechologies.co.ke@gmail.com>
2022-05-06 12:01:31 +0300

    write test to check input description

--- src/editTask.test.js
+++ src/editTask.test.js
@@ -3,10 +3,10 @@
  */
 import Task from './tasks.js';
 import displayTask from './displayTask.js';
- 
- const taskObj = new Task();
 
- describe('Should test for status and content updates', () => {
+const taskObj = new Task();
+
+describe('Should test for status and content updates', () => {
   const updateText = (id, statusCheck) => {
     taskObj.tasks.completed = statusCheck;
     localStorage.setItem('tasks', JSON.stringify(taskObj.tasks));
@@ -16,7 +16,7 @@ import displayTask from './displayTask.js';
     taskObj.tasks.description = textValue;
     localStorage.setItem('tasks', JSON.stringify(taskObj.tasks));
   };
-  
+
   document.body.innerHTML = ` <div class="edit-task" data-key="1"> <ul class="ul-edit" >
   <li class="tick" ><input class="text-input" id="1" type="checkbox"></li>
   <li class"text-value" ></li>
@@ -25,3 +25,26 @@ import displayTask from './displayTask.js';
 <i class="fa-solid fa-trash-can delete-icon"></i>
 <hr>
 </div>`;
+
+  const checkBoxValue = document.querySelector('.text-input');
+  const textValue = document.querySelector('.text-value');
+
+  test('Should edit task description', () => {
+    taskObj.addNewTask(textValue);
+    document.body.innerHTML = '  <div class="todo-list"></div> ';
+    displayTask();
+    expect(taskObj.tasks).toHaveLength(1);
+  });
+
+  test('Should update completed task', () => {
+    taskObj.tasks.completed = false;
+    updateText(1, true);
+    expect(taskObj.tasks[0].completed).toBe(checkBoxValue.checked);
+  });
+
+  test('Should test edit task function', () => {
+    const value = 'Text Value';
+    editText(value, 1);
+    expect.any(Function);
+  });
+});
