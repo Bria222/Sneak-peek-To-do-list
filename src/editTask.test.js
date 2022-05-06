3a35e1b550917c8b77b0335348254eb624f03200
Dinma-Faith <phaytep@gmail.com>
2022-05-06 09:43:43 +0100

    Add js function to editTask.test.js file

--- src/editTask.test.js
+++ src/editTask.test.js
@@ -1,8 +1,8 @@
 /**
  * @jest-environment jsdom
  */
- import Task from './tasks.js';
- import displayTask from './displayTask.js';
+import Task from './tasks.js';
+import displayTask from './displayTask.js';
  
  const taskObj = new Task();
 
@@ -16,3 +16,12 @@
     taskObj.tasks.description = textValue;
     localStorage.setItem('tasks', JSON.stringify(taskObj.tasks));
   };
+  
+  document.body.innerHTML = ` <div class="edit-task" data-key="1"> <ul class="ul-edit" >
+  <li class="tick" ><input class="text-input" id="1" type="checkbox"></li>
+  <li class"text-value" ></li>
+  <li class="remove-btn" id="1"><button >&#8942;</button></li>
+</ul>
+<i class="fa-solid fa-trash-can delete-icon"></i>
+<hr>
+</div>`;
