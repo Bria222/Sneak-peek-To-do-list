c597a82de8d867e22009e8fb48f99c3e979d52a5
MR BREE <briantechologies.co.ke@gmail.com>
2022-05-06 11:38:18 +0300

    wtite test to editing function

--- src/editTask.test.js
+++ src/editTask.test.js
@@ -5,3 +5,14 @@
  import displayTask from './displayTask.js';
  
  const taskObj = new Task();
+
+ describe('Should test for status and content updates', () => {
+  const updateText = (id, statusCheck) => {
+    taskObj.tasks.completed = statusCheck;
+    localStorage.setItem('tasks', JSON.stringify(taskObj.tasks));
+  };
+
+  const editText = (textValue) => {
+    taskObj.tasks.description = textValue;
+    localStorage.setItem('tasks', JSON.stringify(taskObj.tasks));
+  };
